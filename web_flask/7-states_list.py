3937712caccddac55948680e995725678d9c9b61
jmodhiambo <jmodhiambo9346@gmail.com>
2025-02-10 03:27:42 +0300

    States and Cities

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
-"""Starts a Flask web application to display States list"""
+#!/usr/bin/env python3
+"""
+Flask web application to display a list of states.
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,20 +9,20 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Close the current SQLAlchemy Session after each request."""
+    """Closes the storage on teardown"""
     from models import storage
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    """Displays an HTML page with a list of states sorted by name"""
     from models import storage
     from models.state import State
-    """Display a HTML page with the list of all State objects."""
     states = storage.all(State).values()
-    return render_template('7-states_list.html',
-                           states=sorted(states, key=lambda state: state.name))
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
