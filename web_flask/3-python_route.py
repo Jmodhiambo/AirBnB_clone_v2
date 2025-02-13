d780e36a356c15fc8784c56deb13451fd8b5bed5
jmodhiambo <jmodhiambo9346@gmail.com>
2025-02-13 03:49:43 +0300

    Testing 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,39 +1,34 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+"""Flask framework"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+def hello_world():
+    """return hello HBNB"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'"""
+    """return HBNB"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c_is_fun(text):
-    """Displays 'C <text>' with underscores replaced by spaces"""
-    if '_' in text:
-        new_text = text.replace('_', ' ')
-        return f"C {new_text}"
-    return f"C {text}"
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """return text given"""
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_is_cool(text):
-    """Displays 'Python <text>' with underscores replaced by spaces
-    The default value of text is “is cool”
-    """
-    return f"Python {text.replace('_', ' ')}"
+@app.route('/python/', defaults={'text': 'is_cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def display(text):
+    """display “Python ”, followed by the value of the text"""
+    return "Python {}".format(text.replace("_", " "))
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
